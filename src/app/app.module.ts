d712aa443ee038398eb36c7eb512ac5781f15b45
unknown <emailkatrinapo@gmail.com>
2021-05-25 17:09:04 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UserhomeComponent } from './userhome/userhome.component';
+import { AdminhomeComponent } from './adminhome/adminhome.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { MetricsComponent } from './metrics/metrics.component';
+import { BugreportsComponent } from './bugreports/bugreports.component';
+import { UsernavComponent } from './usernav/usernav.component';
+import { AdminnavComponent } from './adminnav/adminnav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserhomeComponent,
+    AdminhomeComponent,
+    ProfileComponent,
+    LeaderboardComponent,
+    MetricsComponent,
+    BugreportsComponent,
+    UsernavComponent,
+    AdminnavComponent
   ],
   imports: [
     BrowserModule,
