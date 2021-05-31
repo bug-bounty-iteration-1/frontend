1d27a04f4368c8504683c9272e10c61f7c3fa127
unknown <emailkatrinapo@gmail.com>
2021-05-31 14:05:14 -0400

    fixed some conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,13 +18,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { ProfilePictureComponent } from './profile/profile-picture/profile-picture.component';
 import { FooterComponent } from './footer/footer.component';
 import { ProfileSolutionsComponent } from './profile/profile-solutions/profile-solutions.component';
-<<<<<<< HEAD
 import { ProfileReportsComponent } from './profile/profile-reports/profile-reports.component';
-=======
 import { RegistrationComponent } from './registration/registration.component';
 
 
->>>>>>> main
 
 @NgModule({
   declarations: [
@@ -42,11 +39,8 @@ import { RegistrationComponent } from './registration/registration.component';
     FooterComponent,
     ProfilePictureComponent,
     ProfileSolutionsComponent,
-<<<<<<< HEAD
     ProfileReportsComponent,
-=======
     RegistrationComponent,
->>>>>>> main
 
   ],
   imports: [
