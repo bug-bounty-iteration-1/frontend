c16a23f2bb4c5b4270f278c9b7bf5c53246ef168
unknown <emailkatrinapo@gmail.com>
2021-06-01 15:43:37 -0400

    filtered bugreports table in profile component

--- src/app/profile/profile-reports/profile-reports.component.ts
+++ src/app/profile/profile-reports/profile-reports.component.ts
@@ -12,13 +12,21 @@ export class ProfileReportsComponent implements OnInit {
 
   bugList: Bug[];
 
+  userName = localStorage.getItem('userName');
+  firstName = localStorage.getItem('firstName');
+  lastName = localStorage.getItem('lastName');
+  user = localStorage.getItem('user');
+  role = localStorage.getItem('role');
+
   constructor(private bServ :BugService ) { }
 
   ngOnInit(): void {
      this.bServ.getAllBugs().subscribe(
       response =>{
       this.bugList = response; 
+
       }); 
+      console.log(this.user);
       console.log(this.bugList);
   }
   reloadCurrentPage() {
